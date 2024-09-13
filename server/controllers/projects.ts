4c91aff2280b3c8b24fe80fee04bdd5382064d5c
Liviu2911 <pirvanliviu29@gmail.com>
2024-09-13 15:39:35 +0300

    backend updated

--- server/controllers/projects.ts
+++ server/controllers/projects.ts
@@ -9,7 +9,7 @@ const projects = async (req: Request, res: Response) => {
 
   let searchQuery = id ? "AND id=$2" : "";
 
-  const { data: projects, error } = await tryFunc(async () => {
+  const { data: projectsRes, error } = await tryFunc(async () => {
     const projectsRes = await auth.query(
       "SELECT * FROM projects WHERE userid=$1 " + searchQuery,
       id ? [userId, id] : [userId]
@@ -17,6 +17,13 @@ const projects = async (req: Request, res: Response) => {
 
     return projectsRes.rows;
   });
+
+  if (error) {
+    return res.status(404).send({
+      session: false,
+      error,
+    });
+  }
 
   const { data: username, error: usernameError } = await tryFunc(async () => {
     const usernameRes = await auth.query(
@@ -26,6 +33,59 @@ const projects = async (req: Request, res: Response) => {
 
     return usernameRes.rows[0].username;
   });
+
+  if (usernameError) {
+    return res.status(404).send({
+      session: false,
+      error: usernameError,
+    });
+  }
+
+  const { data: statusesRes, error: statusError } = await tryFunc(async () => {
+    return (
+      await auth.query(
+        "SELECT * FROM statuses WHERE userid=$1 " + searchQuery,
+        id ? [userId, id] : [userId]
+      )
+    ).rows;
+  });
+
+  if (statusError) {
+    return res.status(404).send({
+      session: false,
+      error: statusError,
+    });
+  }
+
+  const { data: todosRes, error: todoError } = await tryFunc(async () => {
+    return (
+      await auth.query(
+        "SELECT * FROM todos WHERE userid=$1 " + searchQuery,
+        id ? [userId, id] : [userId]
+      )
+    ).rows;
+  });
+
+  if (todoError) {
+    return res.status(404).send({
+      session: false,
+      error: todoError,
+    });
+  }
+
+  const statuses = statusesRes.map((status: Record<string, unknown>) => ({
+    ...status,
+    todos: todosRes.filter(
+      (todo: Record<string, unknown>) => todo.statusid === status.id
+    ),
+  }));
+
+  const projects = projectsRes.map((project: Record<string, unknown>) => ({
+    ...project,
+    statuses: statuses.filter(
+      (status: Record<string, unknown>) => status.projectid === project.id
+    ),
+  }));
 
   return res.send({
     projects,
